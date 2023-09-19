0030c2b39ffdb2784f7afece287f0fc65ddbf4f9
Boudenzin <romildo-rrj@hotmail.com>
2023-09-18 21:22:40 -0300

    Atualização de Projeto v3

--- caixaregistradora2.py
+++ caixaregistradora2.py
@@ -9,15 +9,28 @@ valordeparada = 0
 respostaexecucao = "SIM"
 print ("Mercado Bom Todo")
 while (respostaexecucao == "SIM"):
+    codigoproduto = 7777777777777
     ordemdoproduto += 1
-    codigoproduto = int(input(f"Produto {ordemdoproduto}: "))
-    if (codigoproduto) in codigos:
-        for ordemdalista in range (len(codigos)):
-            if (codigoproduto == codigos[ordemdalista]):
-                print (f"{(codigos[ordemdalista])}, {(produtos[ordemdalista])}, R$ {(precos[ordemdalista])}")
-    precototal += precos[0]
-    respostaexecucao = str.upper(input("Deseja reiniciar outro registro? "))
+    while (codigoproduto not in codigos):
+        codigoproduto = int(input(f"Produto {ordemdoproduto}: "))
+        if (codigoproduto not in codigos):
+            print ("Ops, acho que você errou a numeração do código de barras, tente novamente")
+    for ordemdalista in range (len(codigos)):
+        if (codigoproduto == codigos[ordemdalista]):
+            unidades = int(input(f"Quantas unidades de {produtos[ordemdalista]}: "))
+            print (f"{(codigos[ordemdalista])}, {unidades}x {(produtos[ordemdalista])}, R$ {(precos[ordemdalista])} UN")    
+    precototal += (precos[0] * unidades)
+    respostaexecucao = str.upper(input("Deseja iniciar outro registro? "))
 print (f"Total: R$ {precototal}")
-dinheirorecebido = float(input("Dinheiro: R$ "))
-troco = dinheirorecebido - precototal
-print (f"Troco: R$ {troco:.2f}")
+formadepagamento = str.upper(input("Qual o método de pagamento? (CRÉDITO / DÉBITO / DINHEIRO) "))
+if (formadepagamento == "CRÉDITO") or (formadepagamento == "DÉBITO"):
+    print ("Insira sua senha: ")
+    senha = str(input())
+    while ((len(senha) > 4)):
+        print ("Sua senha é inválida, tente de novo")
+        senha = str(input())
+    print (f"Sua compra de R$ {precototal} foi efetuada com sucesso") 
+else:         
+    dinheirorecebido = float(input("Dinheiro: R$ "))
+    troco = dinheirorecebido - precototal
+    print (f"Troco: R$ {troco:.2f}")
